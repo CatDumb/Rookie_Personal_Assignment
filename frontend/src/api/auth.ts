36cb25550d3cb5151555f21f96ceb4efc38477e5
Nguyễn Minh Khoa <khoa.nguyenbkhcm@hcmut.edu.vn>
2025-05-01 18:19:31 +0700

    Added refresh token (untest, unused) to authen endpoints

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -5,13 +5,26 @@ export interface LoginPayload {
     email: string;
     password: string;
 }
+
 export interface LoginResponse {
     first_name: string;
     last_name: string;
     access_token: string;
+    refresh_token: string;
+    token_type: string;
+}
+
+export interface RefreshTokenResponse {
+    access_token: string;
+    token_type: string;
 }
 
 export function login(payload: LoginPayload) {
     return api.post<LoginResponse>("api/user/login", payload)
+        .then(res => res.data);
+}
+
+export function refreshToken(refreshToken: string) {
+    return api.post<RefreshTokenResponse>("api/user/refresh-token", { refresh_token: refreshToken })
         .then(res => res.data);
 }
